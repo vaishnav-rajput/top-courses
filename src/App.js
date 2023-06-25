088de7a9e1c371bd12ca8f13ebbacb228397a4e4
vaishnav <vaishnav.rajput108@gmail.com>
2023-06-26 02:30:30 +0530

    components setted up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+  <div>
+    <div>
+      <Navbar></Navbar>
+    </div>
+    <div>
+      <Filter></Filter>
+    </div>
+    <div>
+      <Cards></Cards>
+    </div>
+  </div>
+  );
 };
 
 export default App;
